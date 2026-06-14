4af4d1c34c35bc5e4c2fe011b4caab5ec2e82ae0
Rodrigo Rejala <rodrirejala@gmail.com>
2026-06-14 20:12:26 -0300

    use status code constants

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(201)
+	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("save a new snippet..."))
 }
 
